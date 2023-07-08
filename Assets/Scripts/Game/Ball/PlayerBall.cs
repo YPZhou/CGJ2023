f9a0cc7ae7feb1481183bb60314d04f05d367d2e
YPZhou <yunpeng_zhou@163.com>
2023-07-08 08:03:22 +0800

    完成玩家移动控制

--- Assets/Scripts/Game/Ball/PlayerBall.cs
+++ Assets/Scripts/Game/Ball/PlayerBall.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace CGJ2023
 {
 	/// <summary>
@@ -11,6 +13,32 @@ namespace CGJ2023
 
 		protected override void UpdateCore()
 		{
+			var moveDirection = Vector3.zero;
+			if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				moveDirection += Vector3.left;
+			}
+
+			if (Input.GetKey(KeyCode.RightArrow))
+			{
+				moveDirection += Vector3.right;
+			}
+
+			if (Input.GetKey(KeyCode.UpArrow))
+			{
+				moveDirection += Vector3.up;
+			}
+
+			if (Input.GetKey(KeyCode.DownArrow))
+			{
+
+				moveDirection += Vector3.down;
+			}
+
+			transform.position = transform.position + moveDirection.normalized * moveSpeed * Time.deltaTime;
 		}
+
+		[SerializeField]
+		float moveSpeed;
 	}
 }
