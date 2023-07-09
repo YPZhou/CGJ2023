096d3d770c160b69d6b3fa1fed3a9e53a511a6dd
Cataphract@31 <44166626+cAtAphrACtCtl@users.noreply.github.com>
2023-07-09 12:15:04 +0800

    箭头缩放

--- Assets/Scripts/Game/Object/Ball/PlayerBall.cs
+++ Assets/Scripts/Game/Object/Ball/PlayerBall.cs
@@ -4,184 +4,189 @@ using UnityEngine;
 
 namespace CGJ2023
 {
-	/// <summary>
-	/// 玩家角色球
-	/// </summary>
-	public class PlayerBall : BaseBall
-	{
-		Transform DirectionRender;
+    /// <summary>
+    /// 玩家角色球
+    /// </summary>
+    public class PlayerBall : BaseBall
+    {
+        Transform DirectionRender;
 
-		protected override void StartCore()
-		{
-			canPush = true;
+        protected override void StartCore()
+        {
+            canPush = true;
             DirectionRender = transform.GetChild(2);
         }
 
-		protected override void UpdateCore()
-		{
-			//var moveDirection = Vector3.zero;
-			//if (Input.GetKey(KeyCode.LeftArrow))
-			//{
-			//    moveDirection += Vector3.left;
-			//}
+        protected override void UpdateCore()
+        {
+            //var moveDirection = Vector3.zero;
+            //if (Input.GetKey(KeyCode.LeftArrow))
+            //{
+            //    moveDirection += Vector3.left;
+            //}
 
-			//if (Input.GetKey(KeyCode.RightArrow))
-			//{
-			//    moveDirection += Vector3.right;
-			//}
+            //if (Input.GetKey(KeyCode.RightArrow))
+            //{
+            //    moveDirection += Vector3.right;
+            //}
 
-			//if (Input.GetKey(KeyCode.UpArrow))
-			//{
-			//    moveDirection += Vector3.up;
-			//}
+            //if (Input.GetKey(KeyCode.UpArrow))
+            //{
+            //    moveDirection += Vector3.up;
+            //}
 
-			//if (Input.GetKey(KeyCode.DownArrow))
-			//{
+            //if (Input.GetKey(KeyCode.DownArrow))
+            //{
 
-			//    moveDirection += Vector3.down;
-			//}
+            //    moveDirection += Vector3.down;
+            //}
 
-			//transform.position = transform.position + moveDirection.normalized * moveSpeed * Time.deltaTime;
+            //transform.position = transform.position + moveDirection.normalized * moveSpeed * Time.deltaTime;
 
-			//if (transform.position.x < -8.6f)
-			//{
-			//	var rigidBody = GetComponent<Rigidbody2D>();
-			//	if (rigidBody != null)
-			//	{
-			//		rigidBody.AddForce(Vector2.right * Mathf.Abs(rigidBody.velocity.x), ForceMode2D.Impulse);
-			//	}
-			//}
+            //if (transform.position.x < -8.6f)
+            //{
+            //	var rigidBody = GetComponent<Rigidbody2D>();
+            //	if (rigidBody != null)
+            //	{
+            //		rigidBody.AddForce(Vector2.right * Mathf.Abs(rigidBody.velocity.x), ForceMode2D.Impulse);
+            //	}
+            //}
 
-			//if (transform.position.x > 8.6f)
-			//{
-			//	var rigidBody = GetComponent<Rigidbody2D>();
-			//	if (rigidBody != null)
-			//	{
-			//		rigidBody.AddForce(Vector2.left * Mathf.Abs(rigidBody.velocity.x), ForceMode2D.Impulse);
-			//	}
-			//}
+            //if (transform.position.x > 8.6f)
+            //{
+            //	var rigidBody = GetComponent<Rigidbody2D>();
+            //	if (rigidBody != null)
+            //	{
+            //		rigidBody.AddForce(Vector2.left * Mathf.Abs(rigidBody.velocity.x), ForceMode2D.Impulse);
+            //	}
+            //}
 
-			//if (transform.position.y > 3.5f)
-			//{
-			//	var rigidBody = GetComponent<Rigidbody2D>();
-			//	if (rigidBody != null)
-			//	{
-			//		rigidBody.AddForce(Vector2.down * Mathf.Abs(rigidBody.velocity.y), ForceMode2D.Impulse);
-			//	}
-			//}
+            //if (transform.position.y > 3.5f)
+            //{
+            //	var rigidBody = GetComponent<Rigidbody2D>();
+            //	if (rigidBody != null)
+            //	{
+            //		rigidBody.AddForce(Vector2.down * Mathf.Abs(rigidBody.velocity.y), ForceMode2D.Impulse);
+            //	}
+            //}
 
-			//if (transform.position.y < -4.7f)
-			//{
-			//	var rigidBody = GetComponent<Rigidbody2D>();
-			//	if (rigidBody != null)
-			//	{
-			//		rigidBody.AddForce(Vector2.up * Mathf.Abs(rigidBody.velocity.y), ForceMode2D.Impulse);
-			//	}
-			//}
+            //if (transform.position.y < -4.7f)
+            //{
+            //	var rigidBody = GetComponent<Rigidbody2D>();
+            //	if (rigidBody != null)
+            //	{
+            //		rigidBody.AddForce(Vector2.up * Mathf.Abs(rigidBody.velocity.y), ForceMode2D.Impulse);
+            //	}
+            //}
 
-			var rigidBody = GetComponent<Rigidbody2D>();
-			if (rigidBody != null)
-			{
-				if (canPush)
-				{
-					if (Input.GetMouseButtonDown(0))
-					{
-						var pushDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-						rigidBody.AddForce(pushDirection.normalized * pushForce, ForceMode2D.Impulse);
-						canPush = false;
-					}
-				}
+            var rigidBody = GetComponent<Rigidbody2D>();
+            if (rigidBody != null)
+            {
+                if (canPush)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        var pushDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+                        rigidBody.AddForce(pushDirection.normalized * pushForce, ForceMode2D.Impulse);
+                        canPush = false;
+                    }
+                }
 
-				DragAllAttachedBalls(rigidBody.velocity);
+                DragAllAttachedBalls(rigidBody.velocity);
 
-				if (!canPush)
-				{
-					canPush = rigidBody.velocity.sqrMagnitude < 0.01f;
-					if (canPush)
-					{
-						room.FinishCurrentPush();
+                if (!canPush)
+                {
+                    canPush = rigidBody.velocity.sqrMagnitude < 0.01f;
+                    if (canPush)
+                    {
+                        room.FinishCurrentPush();
 
-					}
-				}
-			}
+                    }
+                }
+            }
 
-			if (playerIcon != null)
-			{
-				if (canPush)
-				{
-					playerIcon.transform.localPosition = new Vector3(0f, Mathf.Cos(Time.time * 5f) * 0.25f + 1.5f);
-					playerIcon.color = Color.green;
-				}
-				else
-				{
-					playerIcon.transform.localPosition = new Vector3(0f, 1.5f);
-					playerIcon.color = Color.red;
-				}
-			}
+            if (playerIcon != null)
+            {
+                if (canPush)
+                {
+                    playerIcon.transform.localPosition = new Vector3(0f, Mathf.Cos(Time.time * 5f) * 0.25f + 1.5f);
+                    playerIcon.color = Color.green;
+                }
+                else
+                {
+                    playerIcon.transform.localPosition = new Vector3(0f, 1.5f);
+                    playerIcon.color = Color.red;
+                }
+            }
 
-			DirectionRender.GetComponentInChildren<SpriteRenderer>().enabled = canPush;
-			var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            DirectionRender.LookAt(mousePos, Vector3.forward);
+            DirectionRender.GetComponentInChildren<SpriteRenderer>().enabled = canPush;
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var distance = (mousePos - DirectionRender.position).magnitude - 10;
+            distance = Mathf.Clamp(Mathf.Sqrt(distance), 0.4f, 0.8f);
 
+            DirectionRender.localScale = new Vector3(DirectionRender.localScale.x, distance, DirectionRender.localScale.z);
+            DirectionRender.rotation = Quaternion.LookRotation(mousePos, Vector3.forward);
+
+            Debug.Log($"MousePos:{mousePos} Distance: {distance}");
         }
 
-		bool canPush;
+        bool canPush;
 
-		void DragAllAttachedBalls(Vector3 velocity)
-		{
-			var attachedBalls = room.collectableBalls.Select(obj => obj.GetComponent<CollectableBall>()).Where(ball => ball.IsAttached);
-			foreach (var attachedBall in attachedBalls)
-			{
-				var rigidBody = attachedBall.GetComponent<Rigidbody2D>();
-				rigidBody.velocity = velocity;
-			}
-		}
+        void DragAllAttachedBalls(Vector3 velocity)
+        {
+            var attachedBalls = room.collectableBalls.Select(obj => obj.GetComponent<CollectableBall>()).Where(ball => ball.IsAttached);
+            foreach (var attachedBall in attachedBalls)
+            {
+                var rigidBody = attachedBall.GetComponent<Rigidbody2D>();
+                rigidBody.velocity = velocity;
+            }
+        }
 
-		void OnCollisionEnter2D(Collision2D collision)
-		{
-			var colliderGameObject = collision.gameObject;
-			var ball = colliderGameObject.GetComponent<CollectableBall>();
-			var item = colliderGameObject.GetComponent<BaseItem>();
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            var colliderGameObject = collision.gameObject;
+            var ball = colliderGameObject.GetComponent<CollectableBall>();
+            var item = colliderGameObject.GetComponent<BaseItem>();
 
-			if (ball != null)
-			{
-				var color = ball.BallColor;
-				if (room.ThemeColor == color)
-				{
-					room.OnCollectBall(color);
-					ball.DestroyBall();
-				}
-				else
-				{
-					room.ComboCount = 0;
-					ball.AttachTo(this);
-				}
-				attachedBalls.Add(ball);
-			}
-			else if (item != null)
-			{
-				item.ApplyEffect(this); 
-			}
-		}
+            if (ball != null)
+            {
+                var color = ball.BallColor;
+                if (room.ThemeColor == color)
+                {
+                    room.OnCollectBall(color);
+                    ball.DestroyBall();
+                }
+                else
+                {
+                    room.ComboCount = 0;
+                    ball.AttachTo(this);
+                }
+                attachedBalls.Add(ball);
+            }
+            else if (item != null)
+            {
+                item.ApplyEffect(this);
+            }
+        }
 
-		#region ItemEffects
-		public void ClearAttachedBalls()
-		{
-			foreach (var ball in attachedBalls)
-			{
-				ball.DestroyBall();
-			}
-			attachedBalls.Clear();
-		}
+        #region ItemEffects
+        public void ClearAttachedBalls()
+        {
+            foreach (var ball in attachedBalls)
+            {
+                ball.DestroyBall();
+            }
+            attachedBalls.Clear();
+        }
 
-		#endregion
+        #endregion
 
-		[SerializeField]
-		float pushForce;
+        [SerializeField]
+        float pushForce;
 
-		[SerializeField]
-		SpriteRenderer playerIcon;
+        [SerializeField]
+        SpriteRenderer playerIcon;
 
-		HashSet<CollectableBall> attachedBalls = new();
-	}
+        HashSet<CollectableBall> attachedBalls = new();
+    }
 }
